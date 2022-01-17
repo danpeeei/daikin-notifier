4a2457a6ef71d8bf98cd7e9f115cf9e5badab663
danpeeei <kuro.dogbear@gmail.com>
2022-01-17 22:41:05 +0900

    Add cli.py to run cli

--- daikin_notifier/__main__.py
+++ daikin_notifier/__main__.py
@@ -1,24 +1,4 @@
-from daikin_notifier.api import is_needed_water
-from daikin_notifier.history import already_notified, delete_history, save_history
-from daikin_notifier.line import send_message
-
-
-def main():
-    is_needed = is_needed_water
-    if not is_needed:
-        print("Water need not to be supplied")
-        delete_history()
-        return
-
-    print("Water should be supplied")
-    if already_notified():
-        print("Already Notified")
-    else:
-        # 未通知
-        send_message("給水が必要です")
-        save_history()
-        return
-
+from daikin_notifier.cli import main
 
 if __name__ == "__main__":
     main()
